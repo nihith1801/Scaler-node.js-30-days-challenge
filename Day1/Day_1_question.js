4c1fe5794c3c3941998d597933ec0dc9ac28d404
nihith1801 <122144472+nihith1801@users.noreply.github.com>
2024-02-01 19:47:50 +0530

    Create Day_1_question.js

---
+++ Day1/Day_1_question.js
@@ -0,0 +1,23 @@
+//importing fs module
+const fs=require('fs');
+
+//defining the function
+function readmyfile(filePath)
+{
+    fs.readFile(filePath, 'utf8', (err,data)=>
+    {
+        //if error is found...
+        if (err)
+        {
+            console.error(`File ${err} reading error!!!`);
+        }
+        //else continue and display the file data...
+        else
+        {
+            console.log(`File content:\n${data}`);
+        }
+    });
+}
+
+
+readmyfile('test.txt')
